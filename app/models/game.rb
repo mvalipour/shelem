aa3d8095693b5c6aa8aedd4f7f62b82f1d584c51
Mo Valipour <mo.valipour@deliveroo.co.uk>
2018-12-26 21:45:16 +0000

    :nail_care:

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,7 +8,13 @@ class Game < ApplicationRecord
   enum status: [:not_started, :started, :finished]
 
   def participants
-    @_participants ||= (participants_blob ? JSON.parse(participants_blob).with_indifferent_access : {})
+    @_participants ||= begin
+      participants_blob ?
+        JSON.parse(participants_blob)
+          .transform_values { |v| Participant.new(**v.symbolize_keys) }
+          .to_h :
+        {}
+    end
   end
 
   def add_participant!(user_uid, user_name)
@@ -20,14 +26,14 @@ class Game < ApplicationRecord
     return if participants.key?(user_uid)
     # todo raise if max capacity
 
-    participants[user_uid] = { name: user_name }
+    participants[user_uid] = Participant.new(name: user_name)
   end
 
   def started!
     location, roles = ALL_LOCATIONS.to_a.sample
     roles.shuffle!
-    participants.each_with_index do |(_, hash), ix|
-      hash[:role] = roles[ix]
+    participants.each_with_index do |(_, p), ix|
+      p.role = roles[ix]
     end
 
     super
@@ -36,7 +42,7 @@ class Game < ApplicationRecord
   private
 
   def serialize_participants
-    assign_attributes(participants_blob: participants.to_json)
+    assign_attributes(participants_blob: participants.transform_values(&:to_h).to_json)
   end
 
   def clear_cache
