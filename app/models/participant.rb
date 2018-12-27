613e9aeead933e0f5cc3ce98276f49ae536c6aab
Mo Valipour <mo.valipour@deliveroo.co.uk>
2018-12-27 03:35:07 +0000

    Add localisation + game restart

---
+++ app/models/participant.rb
@@ -0,0 +1,12 @@
+class Participant
+  PROPS = %i(name role admin)
+
+  include ActiveModel::Model
+
+  attr_accessor *PROPS
+
+  def to_h
+    PROPS.each_with_object({}) { |prop, hash| hash[prop] = send(prop) }
+      .compact
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Spyfall
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('node_modules')
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
