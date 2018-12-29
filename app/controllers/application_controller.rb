3f507a9c1b2b08b8989be508d8280d3f8b808b3e
Mo Valipour <mo.valipour@deliveroo.co.uk>
2018-12-29 14:01:40 +0000

    :nail_care:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_default_view_data
+
+  def set_default_view_data
+    @current_user = user_uid
+  end
+
   def user_uid
     cookies.signed[:uid]
   end
