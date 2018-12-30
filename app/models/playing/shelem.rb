c6abbc63d575613bc4279f4fe1cbe17ded7a7dd1
Mo Valipour <mo.valipour@deliveroo.co.uk>
2018-12-30 18:37:39 +0000

    WIP

--- app/models/playing/shelem.rb
+++ app/models/playing/shelem.rb
@@ -1,20 +1,20 @@
 module Playing
   class Shelem
-    attr_reader :player_hands, :spare_cards, :round_lead, :round_suit, :round_cards,
-      :scores
+    attr_reader :player_hands, :spare_cards, :game_suit, :round_lead, :round_suit, :round_cards,
+      :scores, :game_lead, :game_bet
 
-    def deal_cards
+    def initialize(game_lead, bet)
+      @round_lead = @game_lead = lead
+      @game_bet = game_bet
+      @round_cards = []
+      @scores = [0, 0]
+
+      # deal_cards
       Deck.new.tap do |deck|
         deck.shuffle!
         @player_hands = 4.times.map { deck.draw(12) }
         @spare_cards = deck.draw(4)
       end
-
-      # todo: there has to be a round of negotiation for this
-      @round_lead = [0, 1, 2, 3].sample
-      @round_suit = nil
-      @round_cards = []
-      @scores = [0, 0]
     end
 
     def next_to_play
@@ -35,6 +35,7 @@ module Playing
 
       if new_round?
         @round_suit = card.suit
+        @game_suit = card.suit unless game_suit
       else
         # can the player has a card for round_suit but not playing it
         if round_suit && card.suit != round_suit && cards_in_hand.map(&:suit).include?(round_suit)
@@ -50,20 +51,51 @@ module Playing
       round_cards.empty?
     end
 
+    def game_finished?
+      player_hands.all(&:empty?)
+    end
+
     def finish_round
       # find winner
-      hands = 4.times.map{ |i| round_cards[(round_lead + i) % 4] }
-      winner_index = hands.map{ |c| c.suit == round_suit ? c.rank : -1 }.each_with_index.max[1]
-      winner_team = winner_index % 2
+      round_winner_team = round_winner_index % 2
 
       # set round_lead
-      @round_lead = winner_index
+      @round_lead = round_winner_index
 
       # count score
-      scores[winner_team] += round_cards.sum(&:score) + 5
+      scores[round_winner_team] += round_cards.sum(&:score) + 5
 
       # reset round
+      @round_suit = nil
       @round_cards = []
+    end
+
+    def round_winner_index
+      # re-order round cards to align for team members
+      hands = 4.times.map{ |i| round_cards[(round_lead + i) % 4] }
+      hands.map do |c|
+        # has player cut hand?
+        if round_suit != game_suit && c.suit == game_suit
+          # add 100 to ensure it's gonna be higher than any of the round suit ranks
+          c.rank + 100
+        else
+          c.suit == round_suit ? c.rank : -1
+        end
+      end.each_with_index.max[1]
+    end
+
+    def game_winner_index
+      scores.each_with_index.max[1]
+    end
+
+    def game_scores
+      # todo
+      winner = game_winner_index
+      if winner == game_lead
+        scores[game_lead] > game_bet ? game_bet : -game_bet
+      else
+
+      end
     end
   end
 end
