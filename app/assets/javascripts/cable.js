e1076265d3030366d74b438c6c14b29458b8cc03
Mo Valipour <mo.valipour@deliveroo.co.uk>
2019-01-04 14:37:31 +0000

    reload page when disconnected

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -12,6 +12,10 @@ function updateData(data) {
   window.VUE_APP.changeData(JSON.parse(data.body));
 }
 
+function reloadPage() {
+  document.location.reload
+}
+
 ready(() => {
   const game_uid = meta('game-uid');
   const player_index = meta('player-index');
@@ -24,6 +28,6 @@ ready(() => {
 
   App.cable.subscriptions.create(
     { channel: 'GameChannel', game_uid: game_uid, player_index: player_index },
-    { received: updateData }
+    { received: updateData, disconnected: reloadPage }
   );
 });
