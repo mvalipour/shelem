eb888eb2d4983c88d10af66efa42826afabb6696
Mo Valipour <mo.valipour@deliveroo.co.uk>
2019-01-06 18:13:39 +0000

    Add image tooltip

--- app/assets/javascripts/components/player.js
+++ app/assets/javascripts/components/player.js
@@ -3,7 +3,7 @@ const Vue = require('vue');
 Vue.component('player', {
   props: ['data', 'name'],
   template: `
-    <span class='player-card'>
+    <span class='player-card' v-bind:title='data.name'>
       <img v-bind:src="['https://api.adorable.io/avatars/64/', data.uid ,'.png'].join('')" />
       {{ name == false ? '' : data.name }}
     </span>
