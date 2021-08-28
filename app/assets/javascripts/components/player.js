e19a8a458ba4724c9e88ea701de3132c33019c68
| Mo Valipour <valipour@gmail.com>
2021-08-28 10:56:10 +0100

    change avatar source

--- app/assets/javascripts/components/player.js
+++ app/assets/javascripts/components/player.js
@@ -4,7 +4,7 @@ Vue.component('player', {
   props: ['data', 'name'],
   template: `
     <span class='player-card' v-bind:title='data.name'>
-      <img v-bind:src="['https://api.adorable.io/avatars/64/', data.uid ,'.png'].join('')" />
+      <img v-bind:src="['https://avatars.dicebear.com/api/human/', data.uid ,'.svg'].join('')" />
       <slot></slot>
     </span>
   `
